e019f7e177b0de041e1c7ecd3ae96940a5345088
WanNajmi <38000088+WanNajmi@users.noreply.github.com>
2018-05-31 15:49:56 +0800

    thirdAssignment

---
+++ ThirdAssignment/udpClient.java
@@ -0,0 +1,19 @@
+import java.net.*;
+import java.io.*;
+ 
+class Client{
+public static void main(String[] args) throws Exception{
+System.out.println("Server Time >>>>");
+DatagramSocket cs=new DatagramSocket();
+InetAddress ip=InetAddress.getByName("192.168.229.255"); 
+byte[] rd=new byte[100];
+byte[] sd=new byte[100];
+DatagramPacket sp=new DatagramPacket(sd,sd.length,ip,7777);
+DatagramPacket rp=new DatagramPacket(rd,rd.length);
+cs.send(sp);
+cs.receive(rp);
+String time=new String(rp.getData());
+System.out.println(time);
+cs.close();
+}
+}
